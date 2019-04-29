13440bdfd027059db4f68a373000c32107596b5c
cferdinandi <chris@gomakethings.com>
2019-04-29 13:53:22 -0400

    New endpoint

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # GMT EDD WP Rest API
-Add WP Rest API hooks into Easy Digital Downloads. Returns an array of purchase IDs.
+Add WP Rest API hooks into Easy Digital Downloads to get products and subscription data.
 
 ## How to use it
 
-### The Endpoint
+### The Endpoints
 
 ```bash
+# Get purchase details for a user
 /wp-json/gmt-edd/v1/users/<user_email_address>
+
+# Get subscription details for a user and product
+/wp-json/gmt-edd/v1/subscriptions?email=<user_email_address>&id=<product_id>
 ```
 
 ### Making a request with WordPress
@@ -14,8 +18,20 @@ Add WP Rest API hooks into Easy Digital Downloads. Returns an array of purchase 
 You'll need to configure an options menu to get the domain, username, and password for authorization. I recommend using the [Application Passwords](https://wordpress.org/plugins/application-passwords/) plugin with this.
 
 ```php
+// Get all user purchases
 wp_remote_request(
 	rtrim($options['wp_api_url'], '/') . '/wp-json/gmt-edd/v1/users/' . $email,
+	array(
+		'method'    => 'GET',
+		'headers'   => array(
+			'Authorization' => 'Basic ' . base64_encode($options['wp_api_username'] . ':' . $options['wp_api_password']),
+		),
+	)
+);
+
+// Get user subscription data
+wp_remote_request(
+	rtrim($options['wp_api_url'], '/') . '/wp-json/gmt-edd/v1/subscriptions?email=' . $email . '&id=' . $product_id,
 	array(
 		'method'    => 'GET',
 		'headers'   => array(
--- gmt-edd-wp-rest-api.php
+++ gmt-edd-wp-rest-api.php
@@ -5,11 +5,63 @@
  * Plugin URI: https://github.com/cferdinandi/gmt-edd-wp-rest-api/
  * GitHub Plugin URI: https://github.com/cferdinandi/gmt-edd-wp-rest-api/
  * Description: Add WP Rest API hooks into Easy Digital Downloads.
- * Version: 0.4.0
+ * Version: 1.0.0
  * Author: Chris Ferdinandi
  * Author URI: http://gomakethings.com
  * License: GPLv3
  */
+
+	function gmt_edd_get_user_subscriptions($request) {
+
+		// Get request parameters
+		$params = $request->get_params();
+
+		// if no email, throw an error
+		if (empty($params['email']) || !filter_var($params['email'], FILTER_VALIDATE_EMAIL)) {
+			return new WP_Error( 'code', __( 'Not a valid email address', 'edd_for_courses' ) );
+		}
+
+		// If no product ID, throw an error
+		if (empty($params['id'])) {
+			return new WP_Error( 'code', __( 'Missing product ID', 'edd_for_courses' ) );
+		}
+
+		// Get subscription
+		$subscriber = new EDD_Recurring_Subscriber(sanitize_email($params['email']));
+		$subscription = $subscriber->get_subscriptions(wp_filter_nohtml_kses($params['id']));
+
+		// If there's no subscription
+		if (empty($subscription)) {
+			return new WP_REST_Response(array(
+				'status' => 'no_subscriptions'
+			), 200);
+		}
+
+		// Get subscription details
+		$subscription_payments = $subscription[0]->get_child_payments();
+		$response = array(
+			'status' => $subscription[0]->status,
+			'amount' => $subscription[0]->recurring_amount,
+			'created' => date('F j, Y', strtotime( $subscription[0]->created )),
+			'expires' => date('F j, Y', strtotime( $subscription[0]->expiration )),
+			'gateway' => $subscription[0]->gateway,
+			$payments = array(),
+		);
+
+		// Create payments array
+		foreach ( $subscription_payments as $payment ) {
+			$$response['payments'][] = array(
+				'id'     => $payment->ID,
+				'amount' => $payment->total,
+				'date'   => date('F j, Y', strtotime( $payment->date )),
+				'status' => $payment->status_nicename,
+			);
+		}
+
+		// Return success
+		return new WP_REST_Response($response, 200);
+
+	}
 
 	function gmt_edd_get_user_purchases($data) {
 
@@ -19,7 +71,7 @@
 		}
 
 		// Get user purchases
-		$purchases = edd_get_users_purchases($data['email']);
+		$purchases = edd_get_users_purchases(sanitize_email($data['email']));
 
 		// Set up list of purchases
 		$purchase_list = array();
@@ -67,11 +119,10 @@
 
 				$invoice_list[] = array(
 					'id' => $purchase->ID,
-					'date' => get_the_date('F d, Y', $purchase->ID),
+					'date' => get_the_date('F j, Y', $purchase->ID),
 					'total' => $payment->total,
 					'products' => $products
 				);
-
 
 			}
 		}
@@ -86,6 +137,7 @@
 
 
 	function gmt_edd_for_courses_register_routes () {
+
 		register_rest_route('gmt-edd/v1', '/users/(?P<email>\S+)', array(
 			'methods' => 'GET',
 			'callback' => 'gmt_edd_get_user_purchases',
@@ -98,5 +150,14 @@
 				),
 			),
 		));
+
+		register_rest_route('gmt-edd/v1', '/subscriptions', array(
+			'methods' => 'GET',
+			'callback' => 'gmt_edd_get_user_subscriptions',
+			'permission_callback' => function () {
+				return current_user_can( 'edit_theme_options' );
+			},
+		));
+
 	}
 	add_action('rest_api_init', 'gmt_edd_for_courses_register_routes');
